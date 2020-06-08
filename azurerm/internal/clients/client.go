cd28b45e1fdf63e662acd67367e87a8bbf1e75ea
Hussain <humoiz@microsoft.com>
2020-06-07 23:19:38 -0700

    Address code review comments

--- azurerm/internal/clients/client.go
+++ azurerm/internal/clients/client.go
@@ -38,12 +38,12 @@ import (
 	iothub "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/iothub/client"
 	keyvault "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/keyvault/client"
 	kusto "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/kusto/client"
+	lighthouse "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/lighthouse/client"
 	loganalytics "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/loganalytics/client"
 	logic "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/logic/client"
 	machinelearning "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/machinelearning/client"
 	maintenance "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/maintenance/client"
 	managedapplication "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/managedapplications/client"
-	managedservices "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/managedservices/client"
 	managementgroup "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/managementgroup/client"
 	maps "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/maps/client"
 	mariadb "github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/services/mariadb/client"
@@ -118,12 +118,12 @@ type Client struct {
 	IoTCentral         *iotcentral.Client
 	KeyVault           *keyvault.Client
 	Kusto              *kusto.Client
+	Lighthouse         *lighthouse.Client
 	LogAnalytics       *loganalytics.Client
 	Logic              *logic.Client
 	MachineLearning    *machinelearning.Client
 	Maintenance        *maintenance.Client
 	ManagedApplication *managedapplication.Client
-	ManagedServices    *managedservices.Client
 	ManagementGroups   *managementgroup.Client
 	Maps               *maps.Client
 	MariaDB            *mariadb.Client
@@ -199,12 +199,12 @@ func (client *Client) Build(ctx context.Context, o *common.ClientOptions) error 
 	client.IoTCentral = iotcentral.NewClient(o)
 	client.KeyVault = keyvault.NewClient(o)
 	client.Kusto = kusto.NewClient(o)
+	client.Lighthouse = lighthouse.NewClient(o)
 	client.LogAnalytics = loganalytics.NewClient(o)
 	client.Logic = logic.NewClient(o)
 	client.MachineLearning = machinelearning.NewClient(o)
 	client.Maintenance = maintenance.NewClient(o)
 	client.ManagedApplication = managedapplication.NewClient(o)
-	client.ManagedServices = managedservices.NewClient(o)
 	client.ManagementGroups = managementgroup.NewClient(o)
 	client.Maps = maps.NewClient(o)
 	client.MariaDB = mariadb.NewClient(o)
